ce86c7d4e29aabbede02a2620788b13834f4ec43
DenaAlharbi <s202250560@kfupm.edu.sa>
2025-11-19 12:18:03 +0300

    to dos done just delete left

--- 7-2-restful-api/server/models/song.model.js
+++ 7-2-restful-api/server/models/song.model.js
@@ -1,10 +1,5 @@
 import mongoose from "mongoose";
 
-const songSchema = new mongoose.Schema({
-  title:  { type: String, required: true, trim: true },
-  artist: { type: String, required: true, trim: true },
-  year:   { type: Number, min: 1900, max: 2100 }
-}, { timestamps: true });
 
 // db schema
 const songSchema = new mongoose.Schema({
@@ -13,6 +8,5 @@ const songSchema = new mongoose.Schema({
     year:   { type: Number, min: 1900, max: 2100 }
 }, { timestamps: true });
 
-const Song = mongoose.model("Song", songSchema);
 
 export const Song = mongoose.model("Song", songSchema);
